bb166cf7be26cf9e31e2d0b887e8374e92cbfdee
Lucymuthusi <lucymuthusi@gmail.com>
2024-03-14 12:53:38 -0700

    args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,7 @@
 #!/usr/bin/python3
-
 import sys
-
-num_args = len(sys.argv)  # Get the number of arguments
-
-# Print the output message
+num_args = len(sys.argv)
 print(f"Number of argument{('s' if num_args > 1 else '')}: {num_args}")
 print(":" if num_args > 0 else ".")
-
-# Print details of each argument (if any)
 for i in range(1, num_args):
    print(f"{i+1}: {sys.argv[i]}")
