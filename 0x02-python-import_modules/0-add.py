9dbe715d1d771a6ae7da34a41609026c005edc38
Lucymuthusi <lucymuthusi@gmail.com>
2024-03-14 12:46:09 -0700

    0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,11 +1,6 @@
 #!/usr/bin/python3
-
-# Import the add function from add_0.py
-from add_0 import add
-
-# Define variables a and b
-a = 1
-b = 2
-
-# Call the add function and print the result
-print(f"{a} + {b} = {add(a, b)}")
+if __name__ == "__main__":
+    from add_0 import add
+      a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
