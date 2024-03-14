2b1a7d9167ce0690a497a48b5c95dc9449df6018
Lucymuthusi <lucymuthusi@gmail.com>
2024-03-14 12:50:36 -0700

    calc.py

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,20 +1,9 @@
 #!/usr/bin/python3
-
-# Import all functions from calculator_1.py
-from calculator_1 import *  # Assuming all functions are defined here
-
-# Define variables a and b
-a = 10
-b = 5
-
-# Call and print addition result
-print(f"{a} + {b} = {add(a, b)}")
-
-# Call and print subtraction result
-print(f"{a} - {b} = {subtract(a, b)}")
-
-# Call and print multiplication result
-print(f"{a} * {b} = {multiply(a, b)}")
-
-# Call and print division result
-print(f"{a} / {b} = {divide(a, b)}")
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+    a = 10
+    b = 5
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
