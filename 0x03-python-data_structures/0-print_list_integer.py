96d402d37651177e2e937286baddd09b850b60af
Lucymuthusi <lucymuthusi@gmail.com>
2024-03-18 12:18:58 -0700

    0-print_list_integer.py

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def print_list_integer(my_list=[]):
+  """Prints all integers of a list, one per line.
+
+  Args:
+    my_list: A list containing integers (default empty list).
+  """
+
+  for num in my_list:
+    print(num)
+    print()
